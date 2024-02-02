3586ddc87a171aeb65e44f1ddb9cf83dfd8594f6
Shivam Anand <97366446+StellarShivam@users.noreply.github.com>
2024-02-02 11:57:17 +0530

    added allMessages API

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -5,5 +5,6 @@ const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.post("/", isAuth, messageController.sendMessage);
+router.get("/allMessages/:chatId", isAuth, messageController.allMessages);
 
 module.exports = router;
